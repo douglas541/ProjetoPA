53aaf1fb96af7a11cc3b2791c2b18cc55303b842
Douglas Sousa Jorge <douglas.sousa541@hotmail.com>
2022-12-28 20:01:23 -0300

    feat: new IsOnTheGround method

--- ProjetoPA/Assets/Scripts/PlayerMovement.cs
+++ ProjetoPA/Assets/Scripts/PlayerMovement.cs
@@ -17,21 +17,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerRigidBody.freezeRotation = true;
-        VerticalMovement();
-        HorizontalMovement();
+        if (IsOnTheGround())
+        {
+            playerRigidBody.freezeRotation = true;
+            VerticalMovement();
+            HorizontalMovement();
+        }
     }
 
-    void VerticalMovement() {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
+    void VerticalMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
             playerRigidBody.velocity = Vector2.up * jumpSpeed;
         }
     }
 
-    void HorizontalMovement () {
-        if(Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Horizontal") == 1) {
+    void HorizontalMovement()
+    {
+        if (Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Horizontal") == 1)
+        {
             Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
             transform.position += movement * Time.deltaTime * movementSpeed;
         }
+    }
+
+    bool IsOnTheGround()
+    {
+        List<RaycastHit2D> results = new List<RaycastHit2D>();
+
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, new Vector2(1.0f, 1.0f), 0.0f, Vector2.down, 0.1f);
+
+        Debug.Log(results.ToString());
+
+        return hit;
     }
 }
